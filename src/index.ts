3602fb848b201997ae30cb3fb3631cb57b50d56a
Ricardo Souza <devricardo90@gmail.com>
2026-02-28 21:21:02 +0100

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
+// Require the framework and instantiate it
 
-console.log("Hello World");
+// ESM
+import "dotenv/config";
 
+import Fastify from "fastify";
 
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+// Run the ser
+// try ver!
+try {
+  await fastify.listen({ port: process.env.PORT });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
+// Server is now listening on ${address}
